c6fe5028ed67f415d48bedb493bca6c100c434e9
jinhyu2357 <jinhyu2357@gmail.com>
2026-01-30 00:33:30 +0900

    update

--- Eulers_equation/Taylor_Series_prove.py
+++ Eulers_equation/Taylor_Series_prove.py
@@ -2,7 +2,7 @@ from manim import *
 
 class TaylorByIntegration(Scene):
     def construct(self):
-        title = Text("Proof of Taylor series integration", font_size=40)
+        title = Text("Proof of Taylor series by integration", font_size=40)
         self.play(Write(title))
         self.wait(2)
         self.play(FadeOut(title))
@@ -30,9 +30,17 @@ class TaylorByIntegration(Scene):
 
         self.play(FadeOut(step1), FadeOut(step1b))
 
+        self.wait(1)
+
+        note = Text(
+            "In the same way (but more complicated)",
+            font_size=20
+        ).shift(UP*2)
+        self.play(Write(note))
+
         step2 = MathTex(
             r"\int_a^x (x-t)f''(t)\,dt = \frac{(x-a)^2}{2}f''(a) + \int_a^x \frac{(x-t)^2}{2}f'''(t)\,dt"
-        )
+        ).shift(UP*1)
         self.play(Write(step2))
         self.wait(2)
 
@@ -51,6 +59,7 @@ class TaylorByIntegration(Scene):
             r"\int_a^x \frac{(x-t)^{n-1}}{(n-1)!} f^{(n)}(t)\,dt"
             r"= \frac{(x-a)^n}{n!} f^{(n)}(a)"
             r"+ \int_a^x \frac{(x-t)^n}{n!} f^{(n+1)}(t)\,dt"
+            ,font_size=38
         ).next_to(dots, DOWN)
 
         self.play(Write(general))
@@ -71,6 +80,12 @@ class TaylorByIntegration(Scene):
 
         self.play(Transform(final_formula, limit_formula))
         self.wait(2)
+
+        note = Text(
+            "When a = 0",
+            font_size=20
+        ).shift(UP*1)
+        self.play(Write(note))
 
         maclaurin = MathTex(
             r"f(x)=\sum_{n=0}^{\infty}\frac{f^{(n)}(0)}{n!}x^n"
